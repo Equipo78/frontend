9741f5fb4383ee6c95bbab371f5525df9a34784c
Fede Antonelli <antonelli.fd@gmail.com>
2022-12-05 21:10:26 -0300

    ADD: header styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,24 @@
-import styles from './styles.module.scss';
+import logo from '../../assets/logo.svg';
+import bell from '../../assets/nav-icons/notifications.svg';
+import user from '../../assets/nav-icons/user.png';
 
+import styles from './styles.module.scss';
 const Header = () => {
-  return <header className={styles.container}>Header</header>;
+  return (
+    <header className={styles.container}>
+      <div className={styles.boxLogo}>
+        <img alt="logo" className={styles.logo} src={logo} />
+      </div>
+
+      <section className={styles.userPanel}>
+        <img alt="bell" src={bell} />
+        <article className={styles.user}>
+          <img alt="user" src={user} />
+          <p>Hola Maria</p>
+        </article>
+      </section>
+    </header>
+  );
 };
 
 export default Header;
