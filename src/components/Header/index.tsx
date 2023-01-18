c4ce815297722f02abf4352d14586217942ba811
Fede Antonelli <antonelli.fd@gmail.com>
2023-01-17 23:21:57 -0300

    ADD: user endpoint

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,26 @@
+import { useUserDataQuery } from '../../app/api/userApi';
 import logo from '../../assets/logo.svg';
 import bell from '../../assets/nav-icons/notifications.svg';
 import user from '../../assets/nav-icons/user.png';
 
 import styles from './styles.module.scss';
 const Header = () => {
+  const { data, isLoading, error, isSuccess } = useUserDataQuery();
+
+  let content;
+
+  if (isLoading) content = <h3>loading...</h3>;
+
+  if (error) content = <div>Error fetching...</div>;
+
+  if (isSuccess)
+    content = (
+      <article className={styles.user}>
+        <img alt="user" src={user} />
+        <p>Hola {data?.user.name}</p>
+      </article>
+    );
+
   return (
     <header className={styles.container}>
       <div className={styles.boxLogo}>
@@ -12,10 +29,7 @@ const Header = () => {
 
       <section className={styles.userPanel}>
         <img alt="bell" src={bell} />
-        <article className={styles.user}>
-          <img alt="user" src={user} />
-          <p>Hola Maria</p>
-        </article>
+        {content}
       </section>
     </header>
   );
